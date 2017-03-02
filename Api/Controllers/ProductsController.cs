20fc6b9241026c351f7cec9139eaca12ef57c16c
bkoenig <bkoenig@hmbnet.com>
2017-03-02 12:33:31 -0500

    initial commit

---
+++ Api/Controllers/ProductsController.cs
@@ -0,0 +1,130 @@
+﻿using Api.Models;
+using Api.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using System.Web.Http.Description;
+
+namespace Api.Controllers
+{
+    [EnableCorsAttribute("http://localhost:58397", "*", "*")]
+    public class ProductsController : ApiController
+    {
+        [ResponseType(typeof(Product))]
+        // GET: api/Products
+        public IHttpActionResult Get()
+        {
+            try
+            {
+                var productRepository = new ProductRepository();
+                return Ok(productRepository.Retrieve());
+            }
+            catch (Exception e)
+            {
+                return InternalServerError(e);
+            }
+        }
+
+        // GET: api/Products
+        public IEnumerable<Product> Get(string search)
+        {
+            var productRepository = new ProductRepository();
+            var products = productRepository.Retrieve();
+            return products.Where(p => p.ProductCode.Contains(search));
+        }
+
+        [ResponseType(typeof(Product))]
+        [Authorize()]
+        // GET: api/Products/5
+        public IHttpActionResult Get(int id)
+        {
+            try
+            {
+                Product product;
+                var productRepository = new ProductRepository();
+
+                if (id > 0)
+                {
+                    var products = productRepository.Retrieve();
+                    product = products.FirstOrDefault(p => p.ProductId == id);
+                    if (product == null)
+                    {
+                        return NotFound();
+                    }
+                }
+                else
+                {
+                    product = productRepository.Create();
+                }
+                return Ok(product);
+            }
+            catch (Exception e)
+            {
+                return InternalServerError(e);
+            }
+        }
+
+        // POST: api/Products
+        public IHttpActionResult Post([FromBody]Product product)
+        {
+            try
+            {
+                if (product == null)
+                {
+                    return BadRequest("Product cannot be null");
+                }
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest(ModelState);
+                }
+                var productRepository = new ProductRepository();
+                var newProduct = productRepository.Save(product);
+                if (newProduct == null)
+                {
+                    return Conflict();
+                }
+                return Created<Product>(Request.RequestUri + newProduct.ProductId.ToString(), newProduct);
+            }
+            catch (Exception e)
+            {
+                return InternalServerError(e);
+            }
+        }
+
+        // PUT: api/Products/5
+        public IHttpActionResult Put(int id, [FromBody]Product product)
+        {
+            try
+            {
+                if (product == null)
+                {
+                    return BadRequest("Product cannot be null");
+                }
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest(ModelState);
+                }
+                var productRepository = new ProductRepository();
+                var updatedProduct = productRepository.Save(id, product);
+                if (updatedProduct == null)
+                {
+                    return NotFound();
+                }
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                return InternalServerError(e);
+            }
+        }
+
+        // DELETE: api/Products/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
