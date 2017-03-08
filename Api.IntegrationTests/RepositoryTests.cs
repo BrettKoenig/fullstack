c8837361dd5891ae18056bdd0ed8110b33ad09e3
bkoenig <bkoenig@hmbnet.com>
2017-03-08 12:40:42 -0500

    adding an integration tests project and getting one passing

---
+++ Api.IntegrationTests/RepositoryTests.cs
@@ -0,0 +1,24 @@
+﻿using Api.Repositories;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Api.IntegrationTests
+{
+    [TestFixture]
+    public class RepositoryTests
+    {
+        [Test]
+        public void ShouldGetTournaments()
+        {
+            var sut = new TournamentRepository();
+
+            var tournaments = sut.Retrieve();
+
+            Assert.That(tournaments.Count, Is.GreaterThan(0));
+        }
+    }
+}
---
+++ Api.IntegrationTests/TestFixtureLifecycle.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using System;
+using System.Data.Entity;
+
+namespace Api.IntegrationTests
+{
+    [SetUpFixture]
+    public class TestFixtureLifecycle
+    {
+        public TestFixtureLifecycle()
+        {
+            EnsureDataDirectoryConnectionStringPlaceholderIsSet();
+
+            EnsureNoExistingDatabaseFiles();
+        }
+
+        private static void EnsureDataDirectoryConnectionStringPlaceholderIsSet()
+        {
+            AppDomain.CurrentDomain.SetData("DataDirectory", NUnit.Framework.TestContext.CurrentContext.TestDirectory);
+        }
+
+        private void EnsureNoExistingDatabaseFiles()
+        {
+            const string connectionString = "name=DefaultConnection";
+
+            if (Database.Exists(connectionString))
+            {
+                //Database.Delete(connectionString);
+            }
+        }
+    }
+}
