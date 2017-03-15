2fb78bad70d857ee2b7ef7a87e7c88c707d780fd
bkoenig <bkoenig@hmbnet.com>
2017-03-15 15:47:45 -0400

    removing Angular1 project

--- ionicV2/src/shared/api.service.ts
+++ ionicV2/src/shared/api.service.ts
@@ -1,8 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Http, Response, RequestOptions, Headers } from '@angular/http';
 
-//import { AuthService } from '../shared/shared';
-
 import { AuthService } from './auth.service';
 
 import 'rxjs';
